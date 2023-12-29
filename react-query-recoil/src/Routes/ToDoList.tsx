b06d00c0afee15cc57adbb52c134c90ad18448b4
mintcoo <xhaktm3344@gmail.com>
2023-12-29 23:04:01 +0900

    recoil 재시작

--- react-query-recoil/src/Routes/ToDoList.tsx
+++ react-query-recoil/src/Routes/ToDoList.tsx
@@ -1,19 +1,39 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { atom, useRecoilState } from "recoil";
 
 interface IForm {
   toDo: string;
 }
 
+interface IToDo {
+  id: number;
+  text: string;
+  category: "Done" | "Doing" | "ToDo";
+  // 그냥 string이 아니라 딱 저 3가지 카테고리중 하나만 가능하다고 명시
+}
+const toDoState = atom<IToDo[]>({
+  // 타입 적어주는 곳 잘 보자
+  key: "toDo",
+  default: [],
+});
+
 function TodoList() {
-  const [todo, setTodo] = useState<string>("");
+  const [toDos, setToDos] = useRecoilState(toDoState);
 
   const { register, handleSubmit, setValue } = useForm<IForm>();
 
   const onSubmit = (data: IForm) => {
+    // 데이터가 모두 유효하다면
     console.log(data, "data");
+    setToDos((prev) => [
+      { id: Date.now(), text: data.toDo, category: "ToDo" },
+      ...prev,
+    ]);
     setValue("toDo", "");
+    // 입력후 빈 칸으로 만들어주기
   };
+  console.log(toDos);
 
   return (
     <>
@@ -26,6 +46,11 @@ function TodoList() {
         />
         <button className="w-20 bg-pink-300 rounded-lg">add</button>
       </form>
+      <ul>
+        {toDos.map((toDo) => {
+          return <li key={toDo.id}>{toDo.text}</li>;
+        })}
+      </ul>
     </>
   );
 }
