75081097e4c108d3fdbbe874204cf36d46b206c7
mintcoo <xhaktm3344@gmail.com>
2023-12-30 16:41:11 +0900

    recoil selector 시작

--- react-query-recoil/src/Components/atoms.ts
+++ react-query-recoil/src/Components/atoms.ts
@@ -1,4 +1,5 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+// selector는 atom의 output을 변형시키는 것
 // 타입이랑 atom이랑 같이 export 해주자
 
 export interface IToDo {
@@ -12,4 +13,18 @@ export const toDoState = atom<IToDo[]>({
   // 타입 적어주는 곳 잘 보자
   key: "toDo",
   default: [],
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    // atom의 값을 가져옴 atom을 항상 보고있음
+    const toDoList = toDos.filter((toDo) => toDo.category == "ToDo");
+    // 조건에 맞는것만 배열로 반환하는 함수 filter
+    const doingList = toDos.filter((toDo) => toDo.category == "Doing");
+    const doneList = toDos.filter((toDo) => toDo.category == "Done");
+    return [toDoList, doingList, doneList];
+    // 각각을 배열로 만들어서 하나의 배열안에 [[1], [2], [3]] 이런식으로 리턴
+  },
 });
--- react-query-recoil/src/Routes/ToDoList.tsx
+++ react-query-recoil/src/Routes/ToDoList.tsx
@@ -1,22 +1,37 @@
 import { useRecoilValue } from "recoil";
 import CreateToDo from "../Components/CreateToDo";
-import { toDoState } from "../Components/atoms";
+import { toDoSelector, toDoState } from "../Components/atoms";
 import ToDo from "../Components/ToDo";
 
 function TodoList() {
-  const toDos = useRecoilValue(toDoState);
+  // const toDos = useRecoilValue(toDoState);
   // 값만 주시하므로 useRecoilValue 사용
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
+  // 3개의 배열을 리턴중인데 배열을 열고 배열별로 이름을 지정하면 각각 꺼낼수있음
 
   return (
     <>
       <div className="text-4xl">TodoList</div>;
       <CreateToDo />
+      <div className="font-bold text-purple-900 text-4xl">할 일</div>
       <ul>
-        {toDos.map((toDo) => (
+        {toDo.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
         {/* {toDos.map((toDo) => <ToDo text={toDo.text} category={toDo.category} id={toDo.id} />)} */}
         {/* 이렇게 길게 안쓰고 위에처럼 한번에 가능 왜냐면 toDo자체가 전달하는 props와 같은 모양이기때문 */}
+      </ul>
+      <div className="font-bold text-purple-900 text-4xl">하는 중</div>
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <div className="font-bold text-purple-900 text-4xl">끝</div>
+      <ul>
+        {done.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
       </ul>
     </>
   );
